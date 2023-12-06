fade1f7da1a27025760366647e963e56965afaa4
beboopr <linnets-router.09@icloud.com>
2023-12-06 01:59:45 -0500

    repo addition

--- apt-tools/apt-tools.sh
+++ apt-tools/apt-tools.sh
@@ -11,22 +11,26 @@ repos=(
 
 # Define your apt installation tools here
 apt_packages=(
-    "git"
     "curl"
-    "vim"
-    "tilix"
-    "zsh"
-    "tmux"
+    "git"
+    "htop"
+    "neofetch"
     "python3-pip"
-
-
+    "sqlmap"
+    "synaptic"
+    "tcpdump"
+    "tilix"
+    "tmux"
+    "vim"
+    "zsh"
     # ... Add more packages
 )
 
 # Clone GitHub repositories
 echo "Cloning GitHub repositories..."
 for repo in "${repos[@]}"; do
-    git clone https://github.com/$repo.git
+    git clone https://github.com/Z4nzu/hackingtool.git
+    git clone https://github.com/tegal1337/CiLocks
 done
 
 # Install apt packages
